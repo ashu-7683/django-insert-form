e1d84feed90892841fcd0a9e623860db559228d5
ASHUTOSH KHILAR <ashutoshkhilar5@gmail.com>
2025-12-21 20:57:52 +0530

    Update SECRET_KEY in settings.py
    Replaced the secret key with a placeholder for security.

--- djinsertform/settings.py
+++ djinsertform/settings.py
@@ -21,7 +21,7 @@ TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2m3f3a4a-2p6+v!uz!aml=_&hv0&g)s#-=+fje64q+3kke2r&^'
+SECRET_KEY = 'your-secret-key-here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
